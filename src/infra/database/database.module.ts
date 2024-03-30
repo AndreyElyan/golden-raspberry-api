a77850119348c07555f03d94bf03795a60fde1fc
Andrey Elyan <945465@lojasrenner.com.br>
2024-03-29 23:33:13 -0300

    feat: Integrate DatabaseModule and HttpModule, add FindManyMovies use case. Enables use of database methods and API routes for finding multiple movies.

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,16 @@
 import { PrismaMoviesRepository } from './prisma/infra-repositories/prisma-movies-repository';
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
+import { MoviesRepository } from '@domain/domain-repositories/MoviesRepository';
 
 @Module({
-  providers: [PrismaMoviesRepository, PrismaService],
-  exports: [PrismaMoviesRepository],
+  providers: [
+    PrismaService,
+    {
+      provide: MoviesRepository,
+      useClass: PrismaMoviesRepository,
+    },
+  ],
+  exports: [MoviesRepository],
 })
 export class DatabaseModule {}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,9 +2,13 @@ import { DatabaseModule } from '@infra/database/database.module';
 import { PrismaMoviesRepository } from '@infra/database/prisma/infra-repositories/prisma-movies-repository';
 import { PrismaService } from '@infra/database/prisma/prisma.service';
 import { Module } from '@nestjs/common';
+import { MoviesController } from './controllers/movies.controller';
+import { FindManyMovies } from '@app/use-cases/findManyMoviesUseCase';
+import { MoviesRepository } from '@domain/domain-repositories/MoviesRepository';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [PrismaMoviesRepository, PrismaService],
+  providers: [PrismaMoviesRepository, PrismaService, FindManyMovies],
+  controllers: [MoviesController],
 })
 export class HttpModule {}
